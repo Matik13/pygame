123775717cd370d6c8f7b6a33fb228b75482888a
Matik <matiklego@poczta.fm>
2023-03-25 02:01:34 +0100

    Dodano Rycerza który obraca się na boki

--- rpg/main.py
+++ rpg/main.py
@@ -1,13 +1,16 @@
 # Example file showing a circle moving on screen
 import pygame
-
+import os
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1366, 768))
 clock = pygame.time.Clock()
 running = True
+pygame.display.set_caption('RPG Open World By Matik')
 dt = 0
-
+direction = 2
+RYCERZ_IMAGE= pygame.image.load(os.path.join('rpg', 'Rycerz128.png'))
+RYCERZR_IMAGE= pygame.image.load(os.path.join('rpg', 'Rycerz128r.png'))
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
@@ -19,8 +22,11 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("lightblue")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    if(direction == 2):
+        screen.blit(RYCERZ_IMAGE, (player_pos))
+    if(direction == 3):
+        screen.blit(RYCERZR_IMAGE, (player_pos))
+    #pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -29,8 +35,10 @@ while running:
         player_pos.y += 300 * dt
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
+        direction = 2
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+        direction = 3
 
     # flip() the display to put your work on screen
     pygame.display.flip()
